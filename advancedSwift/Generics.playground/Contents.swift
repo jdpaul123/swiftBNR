9995f041f3daafb639bfcd4fa60899abd8c3c27a
Jonathan Paul <jpaul@Jonathans-MacBook-Air.local>
2021-03-26 11:44:42 -0700

    ch 21 done w/o challenges

--- advancedSwift/Generics.playground/Contents.swift
+++ advancedSwift/Generics.playground/Contents.swift
@@ -1,6 +1,15 @@
 import Cocoa
 
-struct Stack<Element> {
+// This is for asociated types
+struct StackIterator<T>: IteratorProtocol {
+    var stack: Stack<T>
+    
+    mutating func next() -> T? {
+        return stack.pop()
+    }
+}
+
+struct Stack<Element>: Sequence {
     var items = [Element]()
     
     mutating func push(_ newItem: Element) {
@@ -19,7 +28,16 @@ struct Stack<Element> {
         }
         return Stack<U>(items: mappedItems)
     }
-
+    
+    func makeIterator() -> StackIterator<Element> {
+        return StackIterator(stack: self)
+    }
+    
+    mutating func pushAll<S: Sequence>(_ sequence: S) where S.Element == Element {
+        for item in sequence {
+            self.push(item)
+        }
+    }
 }
 
 var intStack = Stack<Int>()
@@ -51,3 +69,97 @@ func myMap<T,U>(_ items: [T], _ txform: (T) -> (U)) -> [U] {
 let strings = ["one", "two", "three"]
 let stringLengths = myMap(strings) { $0.count }
 print(stringLengths)
+
+func checkIfEqual<T: Equatable>(_ first: T, _ second: T) -> Bool {
+    return first == second
+}
+
+print(checkIfEqual(1, 1))
+print(checkIfEqual("a string", "a string"))
+print(checkIfEqual("a string", "a different string"))
+
+func checkIfDescriptionsMatch<T: CustomStringConvertible, U: CustomStringConvertible>(_ first: T, _ second: U) -> Bool {
+    return first.description == second.description
+}
+
+print()
+print(checkIfDescriptionsMatch(Int(1), UInt(1)))
+print(checkIfDescriptionsMatch(1, 1.0))
+print(checkIfDescriptionsMatch(Float(1.0), Double(1.0)))
+
+
+// Using asociated types
+var myStack = Stack<Int>()
+myStack.push(10)
+myStack.push(20)
+myStack.push(30)
+
+var myStackIterator = StackIterator(stack: myStack)
+while let value = myStackIterator.next() {
+    print("got \(value)")
+}
+
+for value in myStack {
+    print("for-in loop: got \(value)")
+}
+
+myStack.pushAll([1, 2, 3])
+for value in myStack {
+    print("after pushing: got \(value)")
+}
+
+var myOtherStack = Stack<Int>()
+myOtherStack.pushAll([1, 2, 3])
+myStack.pushAll(myOtherStack)
+for value in myStack {
+    print("after pushing items onto stack, got \(value)")
+}
+
+
+// Generic Composition adn Opague Types
+protocol Food {
+    var menuListing: String { get }
+}
+
+struct Bread: Food {
+    var kind = "sourdough"
+    var menuListing: String {
+        "\(kind) bread"
+    }
+}
+
+func eat<T: Food>(_ food: T) {
+    print("I sure love \(food.menuListing).")
+}
+
+eat(Bread())
+
+struct Restaurant {
+    
+    private struct SlicedFood<Ingredient: Food>: Food {
+        var food: Ingredient
+        var menuListing: String {
+            "a slice of \(food.menuListing)"
+        }
+    }
+    
+    private struct CookedFood<Ingredient: Food>: Food {
+        var food: Ingredient
+        var menuListing: String {
+            "\(food.menuListing), cooked to perfection"
+        }
+    }
+    
+    func makeSlicedBread() -> some Food {
+        return SlicedFood(food: Bread())
+    }
+    
+    func makeToast() -> some Food {
+        let slicedBread = SlicedFood(food: Bread())
+        return CookedFood(food: slicedBread)
+    }
+}
+
+let restaurant = Restaurant()
+let toast = restaurant.makeToast()
+eat(toast)
