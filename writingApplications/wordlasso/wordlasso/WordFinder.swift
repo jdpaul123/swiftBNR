d12ef6772f5b04b422e491f9f69de0a6fc8d1090
Jonathan Paul <jpaul@Jonathans-MacBook-Air.local>
2021-03-28 21:00:57 -0700

    na

--- writingApplications/wordlasso/wordlasso/WordFinder.swift
+++ writingApplications/wordlasso/wordlasso/WordFinder.swift
@@ -10,4 +10,10 @@ import Foundation
 struct WordFinder {
     static let wildcard: Character = "."
     let wordList: [String]
+    
+    private func isMatch(template: String, with word: String) -> Bool {
+        guard template.count == word.count else { return false }
+        
+        return template.indices.allSatisfy{ index in template[index] == WordFinder.wildcard || template[index] == word[index]}
+        }
 }
