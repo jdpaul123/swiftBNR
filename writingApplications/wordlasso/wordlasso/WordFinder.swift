f4935ff66d9bb889e7b7acf2d020036daaa528da
Jonathan Paul <jpaul@Jonathans-MacBook-Air.local>
2021-03-28 17:49:47 -0700

    na

---
+++ writingApplications/wordlasso/wordlasso/WordFinder.swift
@@ -0,0 +1,13 @@
+//
+//  WordFinder.swift
+//  wordlasso
+//
+//  Created by Jonathan Paul on 3/28/21.
+//
+
+import Foundation
+
+struct WordFinder {
+    static let wildcard: Character = "."
+    let wordList: [String]
+}
---
+++ writingApplications/wordlasso/wordlasso/main.swift
@@ -0,0 +1,11 @@
+//
+//  main.swift
+//  wordlasso
+//
+//  Created by Jonathan Paul on 3/28/21.
+//
+
+import Foundation
+
+print("Hello, World!")
+
