6d4bb22320a473971c7db2850deab069542b4e01
Jonathan Paul <jpaul@dyn-10-108-61-212.wless.uoregon.edu>
2021-03-29 18:51:00 -0700

    na

--- writingApplications/wordlasso/wordlasso/main.swift
+++ writingApplications/wordlasso/wordlasso/main.swift
@@ -7,5 +7,38 @@
 
 import Foundation
 
-print("Hello, World!")
+struct Wordlasso {
+    func run() throws {
+        let path = "/usr/share/dict/words"
+        let wordFinder = try WordFinder(wordListPath: path, ignoreCase: true)
+        
+        let args = CommandLine.arguments
+        print("Command-line arguments: \(args)")
+        
+        if args.count > 1 {
+            let template = args[1]
+            findAndPrintMatches(for: template, using: wordFinder)
+        } else {
+            while true {
+                print("Enter word template: ", terminator: "")
+                let template = readLine() ?? ""
+                if template.isEmpty { return }
+                findAndPrintMatches(for: template, using: wordFinder)
+            }
+        }
+    }
+    
+    private func findAndPrintMatches(for template: String, using wordFinder: WordFinder) {
+        let matches = wordFinder.findMatches(for: template)
+        print("Found \(matches.count) \(matches.count == 1 ? "match" : "matches"):")
+        for match in matches {
+            print(match)
+        }
+    }
+}
 
+do {
+    try Wordlasso().run()
+} catch {
+    fatalError("Program exited unexpectedly. \(error)")
+}
